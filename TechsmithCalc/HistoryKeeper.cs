6a98005bf8b7cce5e68f0ebfb26c663c5cd32c11
dport17 <porterd3@udayton.edu>
2021-12-21 14:50:40 -0500

    + HistoryKeeper

---
+++ TechsmithCalc/HistoryKeeper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechsmithCalc
+{
+   class HistoryKeeper
+   {
+      private List<KeyValuePair<string, string>> history;
+
+      public void AddToHistory(string exp, string result)
+      {
+         history.Add(new KeyValuePair<string, string>(exp, result));
+      }
+
+      public void WipeHistory()
+      {
+         //kill the history dictionary
+      }
+   }
+}
